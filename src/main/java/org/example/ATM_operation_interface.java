8137c7e22f879a5c16243ce259a982e4d089c434
Md. Borhan Uz Jamil <borhan.jamil779@gmail.com>
2022-10-31 23:21:21 +0600

    the complete project which will help us to understand all activiteis of an ATM machine

---
+++ src/main/java/org/example/ATM_operation_interface.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface ATM_operation_interface {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
